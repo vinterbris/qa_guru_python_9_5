5d957cf528db87a692c2137625e3486af85ddafd
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-11-26 14:12:08 +0300

    test fully passes

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ from selenium import webdriver
 @pytest.fixture(scope='function', autouse=True)
 def browser_managment():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1080
-    browser.config.window_height = 1920
+    # browser.config.window_width = 1080
+    # browser.config.window_height = 1920
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,35 +1,45 @@
 from selene import browser, be, have, command
+from selenium.webdriver import Keys
 
 
 def test_registration():
     browser.open('/automation-practice-form')
+    # name, email, gender, phone
     browser.element('#firstName').should(be.blank).type('Sergey')
     browser.element('#lastName').should(be.blank).type('Dobrovolskiy')
     browser.element('#userEmail').should(be.blank).type('dobrovolskiy@qa.ru')
     browser.element('#gender-radio-1').should(have.value('Male')).with_(click_by_js=True).click()
     browser.element('#userNumber').should(be.blank).type('1002003040')
-
+    # date of birth
     browser.element('#dateOfBirthInput').click()
-
+    # month
     browser.element('.react-datepicker__month-select').click()
-    browser.all('.react-datepicker__month-select>option').should(have.size(12))
-    browser.all('.react-datepicker__month-select>option').should(have.exact_texts('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
+    browser.all('.react-datepicker__month-select>option').should(
+        have.exact_texts('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                         'October', 'November', 'December'))
     browser.element('.react-datepicker__month-select>option[value="0"]').click()
-
+    # year
     browser.element('.react-datepicker__year-select').click()
     browser.all('.react-datepicker__year-select>option').should(have.size(201))
     browser.all('.react-datepicker__year-select>option').first.should(have.exact_text('1900'))
     browser.all('.react-datepicker__year-select>option')[-1].should(have.exact_text('2100'))
-    # browser.element('.react-datepicker__year-select>option[value="1990"').perform(command.js.scroll_into_view)
-    # browser.element('.react-datepicker__year-select>option[value="2100"').click()
     browser.element('.react-datepicker__year-select').type("2100").press_enter()
-
-    browser.element('#subjectsContainer').click().type('Math').press_enter()
-    # browser.element('.subjects-auto-complete__control').click()
-    # browser.element('.subjects-auto-complete__value-container').type('Math').press_enter()
+    # day
+    browser.element('.react-datepicker__day--002').click()
+    browser.element('#dateOfBirthInput').should(have.value("02 Jan 2100"))
+    # subjects
+    browser.element('#subjectsContainer').click()
+    browser.element('#subjectsInput').type('Math').press_enter()
+    browser.element('#subjectsInput').type('English').press_enter()
+    # hobbies
     browser.element('#hobbies-checkbox-1').with_(click_by_js=True).click()
     browser.element('#hobbies-checkbox-2').with_(click_by_js=True).click()
     browser.element('#hobbies-checkbox-3').with_(click_by_js=True).click()
+    # current addres
     browser.element('#currentAddress').should(be.blank).type('Test Address')
-    ...
-
+    browser.element('#state').perform(command.js.scroll_into_view)
+    browser.element('#state').click()
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#city').click()
+    browser.element('#react-select-4-input').type('Delhi').press_enter()
+    browser.element('#submit').perform(command.js.click)
