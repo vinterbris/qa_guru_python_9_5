61b9851ed737fe4898a739e51ba53a5d9632c13b
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-11-26 15:35:05 +0300

    final fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,11 @@
 import pytest
-import selene
 from selene import browser
 from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_managment():
+def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    # browser.config.window_width = 1080
-    # browser.config.window_height = 1920
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
