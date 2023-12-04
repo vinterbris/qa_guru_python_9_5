fd1c31434b5dac36075dfb06adf85b5c866c6f22
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-12-04 10:25:19 +0300

    gender selector fix

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -10,7 +10,7 @@ def test_registration():
     browser.element('#firstName').should(be.blank).type('Sergey')
     browser.element('#lastName').should(be.blank).type('Dobrovolskiy')
     browser.element('#userEmail').should(be.blank).type('dobrovolskiy@qa.ru')
-    browser.element('#gender-radio-1').should(have.value('Male')).with_(click_by_js=True).click()
+    browser.element('[name=gender][value=Male]').with_(click_by_js=True).click()
     browser.element('#userNumber').should(be.blank).type('1002003040')
 
     browser.element('#dateOfBirthInput').click()
