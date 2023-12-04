9630b270f248cee0347b0f2a6f902212a4f0d9cf
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-12-04 11:18:48 +0300

    proper gender selector

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -10,7 +10,8 @@ def test_registration():
     browser.element('#firstName').should(be.blank).type('Sergey')
     browser.element('#lastName').should(be.blank).type('Dobrovolskiy')
     browser.element('#userEmail').should(be.blank).type('dobrovolskiy@qa.ru')
-    browser.element('[name=gender][value=Male]').with_(click_by_js=True).click()
+    # browser.element('[name=gender][value=Male]').with_(click_by_js=True).click()
+    browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
     browser.element('#userNumber').should(be.blank).type('1002003040')
 
     browser.element('#dateOfBirthInput').click()
